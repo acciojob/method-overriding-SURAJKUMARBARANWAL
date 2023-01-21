66f8373838f5aa1f28df5064bd4fa19f694f4a13
Suraj Kumar <surajbaranwal56@gmail.com>
2023-01-21 14:53:54 +0530

    Add overriding  assignment

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,9 +1,24 @@
 package com.driver;
 
 public class Main {
+    static  class A{
+        String meth(){
+            return "Invoking method from class A";
+        }
 
+    }
+    static  class B extends A {
+        @Override
+        String meth() {
+            return "Method is overridden in Extendend class B";
+        }
+    }
     public  static  void main(String[]args){
-        A odj1=new B();
-        System.out.println(odj1.meth());
+          B b=new B();
+          System.out.println(b.meth());
+          B b1=new B();
+        System.out.println(b1.meth());
+
+
     }
 }
